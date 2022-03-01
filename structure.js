d570e1256c9390d24d52a5b480fc93de03b0b9fd
DikoMolly <99663669+DikoMolly@users.noreply.github.com>
2022-03-01 12:24:47 +0100

    Add files via upload

---
+++ structure.js
@@ -0,0 +1,330 @@
+// let age = 28;
+// let year = 2022;
+
+// console.log(age, year);
+
+// age = 30;
+// year =2025;
+
+// console.log(age,year)
+
+// const name = 40;
+// console.log(name);
+
+// var score = 10;
+// console.log(score);
+// console.log('name')
+
+// let email ='emmanuelnnachi26@gmail.com';
+// console.log(email );
+
+// let firstName ='emmanuel';
+// let lastName = 'nnachi';
+
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+// firstName = 'jude';
+// console.log(firstName);
+
+// console.log(firstName[3]);
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+
+// let result = fullName.toLowerCase();
+// console.log(result,fullName);
+// let mail = email.indexOf('n');
+// console.log(mail);
+
+// let juice = 'emmanuelnnachi26@gmail.com';
+// // let all = juice.lastIndexOf('n');
+// let full = juice.indexOf('n');
+
+// console.log(full);
+
+// let radius = 10;
+// const pi = 3.14;
+// // console.log(radius,pi);
+// // console.log(10/2);
+// // let circle = radius % 3;
+// // let circle = pi* radius**2;
+// // console.log(circle);
+// let like = 10;
+// like*=2**2;
+// let result = `diko has ${like} likes`;
+// console.log(result);
+
+
+// arrays
+
+
+// let diko = ['emma','tolu','ayo','dani-ray'];
+// diko[3]='lenovo';
+// console.log(diko[3]);
+// console.log(diko.join(' '));
+
+
+// let ages = [20,25,30];
+// ages[1] = 40;
+// console.log(ages[1]);
+
+
+// let house = ['emma','ayo','aboy','tolu',20,30,40,50];
+// console.log(house);
+
+// console.log(house[5]);
+
+
+// let slave = ['chinasa','tolu','ayo','diko'];
+// console.log(slave.length);
+// console.log(slave[2]);
+
+
+// // 
+// let result = slave.push('mick','lick','wick');
+// // result = slave.pop();
+// console.log(slave);
+
+
+// let email = 'emmanuelnnachi26@gmail.com';
+// let name = ['emma','diko','tolu'];
+
+// let result = name.includes('tolu');
+// console.log(result);
+
+// let number = '30';
+// number = String(number);
+
+// console.log(number === 30);
+// console.log(number,typeof number);
+// console.log(number !== 30);
+// console.log(number != 35);
+// console.log (number > 35);
+// console.log (number > 35);
+// console.log(number <= 20);
+
+// let name = '100';
+
+// name = Boolean(0);
+
+// console.log(name,typeof name);
+
+
+//  for loops
+
+
+// for (let i = 0; i < 5; i++){
+// 	console.log(i);
+// }
+
+
+// const roll = ['daniel','favour','emmanuel','mercy','rita','rosemary','jude'];
+
+// for (let m = 0; m < roll.length; m++){
+// 	console.log(roll[m]);
+
+
+
+
+// const name = 'emmanuel';
+
+// if(name.length >= 8 && name.includes('@') ){
+ // 	console.log('strongest too long');
+// }
+// else if (name.length <= 9 || name.include('@') || name.length > 20){
+// 	console.log('mighty strong name');
+// }
+// else{
+// 	console.log('name too short')
+// }
+
+
+// let time = 6;
+// time = '5';
+// time = Number(time);
+
+// if(time * 5 === 35){
+// 	console.log('good afternoon');
+// }
+// else{
+// 	console.log('good day');
+// }
+
+
+// let password = 'emmanue@l';
+
+// if(password.length > 10){
+// 	console.log('strong password');
+// }
+// else if (password.length >= 8 && password.includes('@')){
+// 	console.log('password too mighty');
+// }
+// else{
+// 	console.log('password too short');
+// }
+
+// let name = 'emmanuel';
+// if(name == 'daniel'){
+// 	console.log('true')
+// }
+
+
+const scores = [50,20,30,40,10,80,5];
+
+for(let i = 0; i < scores.length; i++){
+	console.log(scores[i]);
+
+if(scores[i] == 50){
+	console.log('loop finished');
+     break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+openNav = ()=>{
+	let list = document.getElementById("links");
+
+	if (list.style.display==="block") {
+		list.style.display = "none";
+	} else {
+		list.style.display = "block";
+
+	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
